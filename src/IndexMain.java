665c276ba7e57337cc415ab9b82201981e08f1cc
Ian O Regan <Ian.ORegan@students.ittralee.ie>
2019-11-04 11:32:54 +0000

    Initial commit

---
+++ src/IndexMain.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.*;
+
+public class IndexMain {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hit return to continue");//To give time for VisualVM to open
+
+        InvertedIndex index = new InvertedIndex();
+
+        File f = new File("C:\\Test");
+        List<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+
+        index.buildIndex(files);
+        
+        index.print();  //output index
+        long sTime =System.currentTimeMillis();
+        index.searchAllWords();
+        long eTime =System.currentTimeMillis();
+        long duration = eTime-sTime;
+        System.out.println("Time: " + duration);
+    }
+}
